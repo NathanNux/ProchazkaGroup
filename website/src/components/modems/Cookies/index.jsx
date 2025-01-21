a7bd73f549258bb812734e1333873ff0d67d92b8
NathanNux <139380259+NathanNux@users.noreply.github.com>
2025-01-21 23:17:43 +0100

    updated scss, plus cookies provider and base for cookies

---
+++ website/src/components/modems/Cookies/index.jsx
@@ -0,0 +1,12 @@
+export default function CookiesBar () {
+    return (
+        <div className="cookies__bar">
+            <div className="cookies__bar__text">
+
+            </div>
+            <div className="cookies__bar__buttons">
+
+            </div>
+        </div>
+    )
+}
